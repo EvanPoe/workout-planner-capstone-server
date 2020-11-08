088c3bc044537a80355c1a7b0f19c2fff06f8329
Evan Poe <evanpoe91@yahoo.com>
2020-11-07 18:32:24 -0600

    Live DB_URL updated

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/workout-planner-capstone',
+  DB_URL: process.env.DB_URL || 'postgres://bvjlvgwdedtytz:9b8fe9e143ca0c5520af1b090e09ea54755695c1e8bf18b82e72e11a854a5126@ec2-75-101-232-85.compute-1.amazonaws.com:5432/d2t0fvmbq83863',
   JWT_SECRET: process.env.JWT_SECRET || 'dog'
 } //what is the rule in which we convert the password into its encrypted form
 //encrypt JW token with 'dog' -- encryption key
